e96f1ad353276acd26dc85170dfddd8c5eeb3efc
Mateusz Szynawa <32458321+mszynawa@users.noreply.github.com>
2017-11-21 17:21:03 +0100

    [Garden War] Menu path.

--- Garden War/Assets/Scripts/LevelManager.cs
+++ Garden War/Assets/Scripts/LevelManager.cs
@@ -8,7 +8,12 @@ public class LevelManager : MonoBehaviour {
 
     private void Start()
     {
-        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        if (autoLoadNextLevelAfter == 0) {
+            Debug.Log("Level auto load disabled!");
+        } else {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+        
     }
 
     public void LoadLevel(string name)
