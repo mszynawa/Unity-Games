9ffa948cc078181c3aeeee1f2042185234abc959
Mateusz Szynawa <32458321+mszynawa@users.noreply.github.com>
2017-11-28 16:06:30 +0100

    [Garden War] Reacting attackers on deffenders
    Adding:
    - fox jumping over grave stone,
    - fox attacking gnome,
    - lizard attacking each defenders

--- Garden War/Assets/Scripts/Attacker.cs
+++ Garden War/Assets/Scripts/Attacker.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
-    [Range (-1f, 1.5f)]
-    public float currentSpeed;
+    private float currentSpeed;
+    private GameObject currentTarget;
 
-	// Use this for initialization
-	void Start () {
-        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
-        myRigidbody.isKinematic = true;
-	}
-	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
@@ -31,5 +26,10 @@ public class Attacker : MonoBehaviour {
     public void StrikeCurrentTarget(float damage)
     {
         Debug.Log(name + " caused damage: " + damage);
+    }
+
+    public void Attack(GameObject obj)
+    {
+        currentTarget = obj;
     }
 }
