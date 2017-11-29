e2c6c8414e8aeb28e9f812ecea69891b6bed423a
Mateusz Szynawa <32458321+mszynawa@users.noreply.github.com>
2017-11-29 16:00:45 +0100

    [Garden War] Adding shredder and simple fire script.

--- Garden War/Assets/Scripts/Defender.cs
+++ Garden War/Assets/Scripts/Defender.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void OnTriggerEnter2D()
-    {
-        Debug.Log(name + " trigger enter");
-    }
+	//only tag
 }
