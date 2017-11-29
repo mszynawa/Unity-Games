d72bd7a57265ad76a00c3417e1c591c25949e26a
Mateusz Szynawa <32458321+mszynawa@users.noreply.github.com>
2017-11-29 18:01:56 +0100

    [Game War] Adding button panel with "defenders shop".

--- Garden War/Assets/Scripts/Attacker.cs
+++ Garden War/Assets/Scripts/Attacker.cs
@@ -24,7 +24,7 @@ public class Attacker : MonoBehaviour {
 
     void OnTriggerEnter2D()
     {
-        Debug.Log(name + " trigger enter");
+        //Debug.Log(name + " trigger enter");
     }
 
     public void SetSpeed(float speed)
---
+++ Garden War/Assets/Scripts/Button.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public GameObject defenderPrefab;
+    public static GameObject selectedDefender;
+
+    private Button[] buttonArray;
+
+    void Start()
+    {
+        buttonArray = GameObject.FindObjectsOfType<Button>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        //print(name + " pressed");
+
+        foreach(Button thisButton in buttonArray)
+        {
+            thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+        
+        GetComponent<SpriteRenderer>().color = Color.white;
+        selectedDefender = defenderPrefab;
+        //print(selectedDefender);
+    }
+}
