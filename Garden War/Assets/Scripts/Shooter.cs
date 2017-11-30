6f1a8236f4a4011960b6834dc945d7a3317678bc
Mateusz Szynawa <32458321+mszynawa@users.noreply.github.com>
2017-11-30 17:33:50 +0100

    [Garden War] Adding code where shooter detect attackers.

--- Garden War/Assets/Scripts/Shooter.cs
+++ Garden War/Assets/Scripts/Shooter.cs
@@ -7,9 +7,13 @@ public class Shooter : MonoBehaviour {
     public GameObject projectile, gun;
 
     private GameObject projectileParent;
+    private Animator animator;
+    private Spawner myLaneSpawner;
 
     void Start()
     {
+        animator = GameObject.FindObjectOfType<Animator>();
+
         projectileParent = GameObject.Find("Projectiles");
 
         if (!projectileParent)
@@ -17,6 +21,49 @@ public class Shooter : MonoBehaviour {
             projectileParent = new GameObject();
             projectileParent.name = "Projectiles";
         }
+
+        SetMyLaneSpawner();
+        //print(myLaneSpawner);
+    }
+
+    void Update()
+    {
+        if (IsAttackerAheadInLane()) {
+            animator.SetBool("isAttacking", true);
+        } else {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    void SetMyLaneSpawner() {
+        Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+
+        foreach (Spawner spawner in spawnerArray) {
+            if (spawner.transform.position.y == transform.position.y) {
+                myLaneSpawner = spawner;
+                return;
+            }
+        }
+
+        Debug.LogError(name + " can't find spawner in lane.");
+    }
+
+    bool IsAttackerAheadInLane()
+    {
+        // Exit if no attackers in lane
+        if (myLaneSpawner.transform.childCount <= 0) {
+            return false;
+        }
+            
+        // If there are attackers, are they ahead?
+        foreach (Transform attackers in myLaneSpawner.transform) {
+            if (attackers.transform.position.x > transform.position.x) {
+                return true;
+            }
+        }
+
+        // Attacker in lane, but behind us.
+        return false;
     }
 
     private void Fire()
